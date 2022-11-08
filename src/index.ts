87943b59093e79efe5935f9d8951ebbb3c0d3ee0
Eduardo Soto <esotom92@gmail.com>
2022-11-08 10:52:58 -0300

    fix: moduke resolution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD')
+import { cotalkerAPI } from './lib/CotalkerAPI'
+
+
+async function main() {
+    const chile = await cotalkerAPI.getProperty('636967d9a3554a0008a414e9') // Pedir un elemento
+    console.log(chile)
+}
+
+main()
